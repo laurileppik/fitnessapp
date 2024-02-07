65c5d43994146f41ae9745eaf31811bb41e5cf65
lauri.leppik <laurile02@gmail.com>
2024-02-07 14:24:54 +0200

    doing HBox witchcraft

--- SaleSystemGUI/src/main/java/fitnesssystem/ui/controllers/DashboardController.java
+++ SaleSystemGUI/src/main/java/fitnesssystem/ui/controllers/DashboardController.java
@@ -5,9 +5,11 @@ import fitnesssystem.logic.ActivityLogic;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -15,6 +17,7 @@ import javafx.scene.text.FontWeight;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
@@ -40,14 +43,43 @@ public class DashboardController implements Initializable {
             Label nameLabel=new Label(activity.getAthlete().getFirstName() + " " + activity.getAthlete().getLastName());
             Label dateAndLocationLabel=new Label(activity.getDateAndLocationOfActivity());
             Label titleLabel=new Label(activity.getTitle());
-            Label distanceLabel = new Label("Distance: " + activity.getDistance() + " km");
-            Label timeLabel = new Label("Time: " + activity.getNormalizedDuration());
+            Label distanceTitleLabel=new Label("Distance ");
+            Label timeTitleLabel=new Label("Time ");
+            Label elevationOrPaceLabelTitle=new Label();
+            Label distanceLabel = new Label(activity.getDistance() + " km");
+            Label timeLabel = new Label(activity.getNormalizedDuration());
+            Label elevationOrPaceLabel=new Label();
             Label emptyRow=new Label();
+            if (Objects.equals(activity.getSport().getName(), "Bike")) {
+                elevationOrPaceLabelTitle.setText("Elevation");
+                elevationOrPaceLabel.setText(String.valueOf(activity.getElevation()) + " m");
+            } else {
+                elevationOrPaceLabelTitle.setText("Pace");
+                double paceInSecondsPerKm = activity.getDuration().getSeconds() / activity.getDistance();
+                int paceMinutes = (int) paceInSecondsPerKm / 60;
+                int paceSeconds = (int) paceInSecondsPerKm % 60;
+                String paceFormatted = String.format("%d:%02d /km", paceMinutes, paceSeconds);
+                elevationOrPaceLabel.setText(paceFormatted);
+            }
 
             styleActivityLabels(nameLabel,titleLabel);
 
-            activitiesContainer.getChildren().addAll(nameLabel,dateAndLocationLabel,titleLabel,distanceLabel, timeLabel,emptyRow);
+            HBox metricsTitlesBox = createHBox(distanceTitleLabel, timeTitleLabel, elevationOrPaceLabelTitle);
+            HBox metricsBox = createHBox(distanceLabel, timeLabel, elevationOrPaceLabel);
+
+            activitiesContainer.getChildren().addAll(nameLabel,dateAndLocationLabel,titleLabel,metricsTitlesBox,metricsBox,emptyRow);
         }
+    }
+
+    private HBox createHBox(Label... labels) {
+        HBox hbox = new HBox();
+        for (Label label : labels) {
+            HBox wrapper = new HBox(label);
+            HBox.setHgrow(wrapper, Priority.ALWAYS);
+            hbox.getChildren().add(wrapper);
+        }
+        hbox.setAlignment(Pos.CENTER);
+        return hbox;
     }
 
     private void styleActivityLabels(Label nameLabel,Label titleLabel) {
